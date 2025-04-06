be15e25db95315d54496e6c203ad32a5266155ed
sp0389 <seventh.theory@protonmail.com>
2025-04-07 04:43:40 +1000

    fix: increased the size of the description box for easier to read input

--- XraySocialClub/Areas/Administration/Models/Announcement/AnnouncementViewModel.cs
+++ XraySocialClub/Areas/Administration/Models/Announcement/AnnouncementViewModel.cs
@@ -8,7 +8,7 @@ namespace XraySocialClub.Areas.Administration.Models.Announcement
         [StringLength(50, ErrorMessage = "Title is too long.")]
         public string Title { get; set; } = default!;
         public DateTime Date { get; set; } = DateTime.UtcNow;
-        public IFormFile? Image { get; set;} = default!;
+        public IFormFile? Image { get; set; } = default!;
         public string ImageUrl { get; set; } = "No image specified.";
         [Required]
         public string Description { get; set; } = default!;
